9afdbd8ec6f77574954e17470e50b51b315979b7
zq <290353142@qq.com>
2025-11-24 11:49:51 +0800

    test

---
+++ scripts/test-svg2png.ts
@@ -0,0 +1,40 @@
+import sharp from 'sharp';
+import path from 'node:path'
+import fs from 'node:fs/promises'
+
+const __dirname = import.meta.dirname;
+const root = path.join(__dirname, 'svgs');
+sharp(path.join(root, 'rect.svg'))
+    .resize(200)
+    .png()
+    .toFile(path.join(__dirname, 'output.png'), (err, info) => {
+        if (err) console.error(err);
+    });
+
+
+async function svgToPng(svg:string) {
+    const puppeteer = await import("puppeteer");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 200,
+        height: 200,
+        deviceScaleFactor: 1 // 可以设置为 2 来获得 Retina 分辨率的图片
+    });
+    await page.setContent(`
+        <!DOCTYPE html>
+        <html>
+            <body style="margin: 0; padding: 0;">
+                <div style="width: ${200}px; height: ${200}px;">
+                    ${svg}
+                </div>
+            </body>
+        </html>`);
+    const png = await page.screenshot({ type: "png" });
+    await browser.close();
+    return png;
+}
+
+fs.readFile(path.join(root, 'rect.svg'),'utf-8')
+.then(svgToPng)
+.then(d=>fs.writeFile(path.join(__dirname, 'output2.png'),d))
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,14 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <MyIcon icon="my:circle"></MyIcon>
       <MyIcon icon="my:rect"></MyIcon>
     </div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    </div>
 </template>
 
 <style scoped>
